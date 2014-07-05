09857ee90b3f44f27e8d1ab1a664294b00572618
Alexander Teinum <ateinum@gmail.com>
2014-07-05 14:31:06 +0200

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+var es6ify = require('es6ify');
+
+var paths = {
+    styles: ['app.less'],
+    scripts: ['app.js'],
+    scriptsWatch: ['*.js']
+};
+
+gulp.task('styles', function() {
+    return gulp.src(paths.styles)
+        .pipe(less())
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dest'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(paths.scripts)
+        .pipe(browserify({transform: es6ify, insertGlobals: true}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dest'));
+});
+
+gulp.task('default', ['styles', 'scripts']);
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch(paths.styles, ['styles']);
+    gulp.watch(paths.scriptsWatch, ['scripts']);
+});
