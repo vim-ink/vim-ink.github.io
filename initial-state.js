348724dc159ee6ef87e7c5f75fe55bf1d2ad8b12
Alexander Teinum <ateinum@gmail.com>
2014-07-27 20:23:29 +0200

    Remove whitespace

--- initial-state.js
+++ initial-state.js
@@ -103,7 +103,7 @@ var initialState = {
         LineNr: {color: '#cccccc'},
         NonText: {color: '#cccccc'},
         SpecialKey: {color: '#cccccc'},
- 
+
         StatusLine: {color: '#777777', backgroundColor: '#eeeeee'},
         StatusLineNC: {color: '#bbbbbb', backgroundColor: '#eeeeee'},
         TabLine: {color: '#bbbbbb', backgroundColor: '#eeeeee'},
