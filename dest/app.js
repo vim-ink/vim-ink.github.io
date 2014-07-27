bbb53e0be1c509b9dd3ddcdb95ad3c30f2c83e7b
Alexander Teinum <ateinum@gmail.com>
2014-07-27 23:37:32 +0200

    Add and refine default color groups

--- dest/app.js
+++ dest/app.js
@@ -1239,7 +1239,7 @@ var App = require('./components/app');
 React.renderComponent(App(), document.body);
 
 
-}).call(this,require("IrXUsu"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_ccdd36ad.js","/")
+}).call(this,require("IrXUsu"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_3c2992b2.js","/")
 },{"./components/app":1,"IrXUsu":20,"buffer":17,"es6ify/node_modules/traceur/bin/traceur-runtime":16,"react":156}],10:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 "use strict";
@@ -1796,7 +1796,7 @@ var initialState = {
   activeFile: 'html',
   activePane: 'light',
   exportName: 'my-default',
-  exportedSource: undefined,
+  ExportedSource: undefined,
   postProcess: {
     dark: {
       brightness: 0,
@@ -1823,85 +1823,51 @@ var initialState = {
     export_: 'show',
     dangerZone: 'hide'
   },
-  dark: {
-    Normal: {
-      color: '#aaaaaa',
-      backgroundColor: '#000000'
-    },
-    Cursor: {highlight: 'reverse'},
-    IncSearch: {backgroundColor: '#555555'},
-    MatchParen: {backgroundColor: '#555555'},
-    Search: {backgroundColor: '#444444'},
-    Visual: {backgroundColor: '#555555'},
-    Conditional: {color: '#999999'},
-    Special: {color: '#999999'},
-    Statement: {color: '#999999'},
-    StorageClass: {color: '#999999'},
-    Type: {color: '#999999'},
-    Directory: {color: '#999999'},
-    Constant: {color: '#777777'},
-    Number: {color: '#777777'},
-    String: {color: '#777777'},
-    Comment: {color: '#555555'},
-    LineNr: {color: '#555555'},
-    NonText: {color: '#555555'},
-    SpecialKey: {color: '#555555'},
-    StatusLine: {
-      color: '#999999',
-      backgroundColor: '#222222'
-    },
-    StatusLineNC: {
-      color: '#777777',
-      backgroundColor: '#222222'
-    },
-    TabLine: {
-      color: '#777777',
-      backgroundColor: '#222222'
-    },
-    TabLineFill: {backgroundColor: '#222222'},
-    TabLineSel: {
-      color: '#999999',
-      backgroundColor: '#222222'
-    },
-    VertSplit: {
-      color: '#222222',
-      backgroundColor: '#222222'
-    },
-    CursorLine: {backgroundColor: '#222222'},
-    CursorColumn: {backgroundColor: '#222222'},
-    CursorLineNr: {color: '#777777'},
-    ColorColumn: {backgroundColor: '#222222'},
-    Pmenu: {backgroundColor: '#444444'},
-    PmenuSel: {backgroundColor: '#555555'},
-    ErrorMsg: {color: '#cc3333'},
-    MoreMsg: {},
-    ModeMsg: {},
-    WarningMsg: {},
-    Todo: {highlight: 'standout'}
-  },
   light: {
+    ColorColumn: {backgroundColor: '#eeeeee'},
+    Comment: {color: '#cccccc'},
+    Conceal: {color: '#777777'},
+    Conditional: {color: '#777777'},
+    Constant: {color: '#999999'},
+    Cursor: {highlight: 'reverse'},
+    CursorColumn: {backgroundColor: '#eeeeee'},
+    CursorLine: {backgroundColor: '#eeeeee'},
+    CursorLineNr: {color: '#999999'},
+    DiffAdd: {},
+    DiffChange: {},
+    DiffDelete: {color: '#cc3333'},
+    DiffText: {},
+    Directory: {color: '#777777'},
+    Error: {},
+    ErrorMsg: {color: '#cc3333'},
+    FoldColumn: {color: '#cccccc'},
+    Folded: {color: '#cccccc'},
+    Ignore: {},
+    IncSearch: {backgroundColor: '#dddddd'},
+    LineNr: {color: '#cccccc'},
+    MatchParen: {backgroundColor: '#dddddd'},
+    ModeMsg: {},
+    MoreMsg: {},
+    NonText: {color: '#cccccc'},
     Normal: {
       color: '#000000',
       backgroundColor: '#ffffff'
     },
-    Cursor: {highlight: 'reverse'},
-    IncSearch: {backgroundColor: '#cccccc'},
-    MatchParen: {backgroundColor: '#cccccc'},
-    Search: {backgroundColor: '#dddddd'},
-    Visual: {backgroundColor: '#cccccc'},
-    Conditional: {color: '#777777'},
-    Special: {color: '#777777'},
-    Statement: {color: '#777777'},
-    StorageClass: {color: '#777777'},
-    Type: {color: '#777777'},
-    Directory: {color: '#777777'},
-    Constant: {color: '#999999'},
     Number: {color: '#999999'},
-    String: {color: '#999999'},
-    Comment: {color: '#cccccc'},
-    LineNr: {color: '#cccccc'},
-    NonText: {color: '#cccccc'},
+    Pmenu: {backgroundColor: '#eeeeee'},
+    PmenuSbar: {},
+    PmenuSel: {backgroundColor: '#dddddd'},
+    PmenuThumb: {},
+    Question: {},
+    Search: {backgroundColor: '#eeeeee'},
+    SignColumn: {color: '#cccccc'},
+    Special: {color: '#777777'},
     SpecialKey: {color: '#cccccc'},
+    SpellBad: {color: '#cc3333'},
+    SpellCap: {},
+    SpellLocal: {},
+    SpellRare: {},
+    Statement: {color: '#777777'},
     StatusLine: {
       color: '#777777',
       backgroundColor: '#eeeeee'
@@ -1910,6 +1876,8 @@ var initialState = {
       color: '#bbbbbb',
       backgroundColor: '#eeeeee'
     },
+    StorageClass: {color: '#777777'},
+    String: {color: '#999999'},
     TabLine: {
       color: '#bbbbbb',
       backgroundColor: '#eeeeee'
@@ -1919,21 +1887,91 @@ var initialState = {
       color: '#777777',
       backgroundColor: '#eeeeee'
     },
-    VertSplit: {
-      color: '#eeeeee',
-      backgroundColor: '#eeeeee'
-    },
-    CursorLine: {backgroundColor: '#eeeeee'},
-    CursorColumn: {backgroundColor: '#eeeeee'},
-    CursorLineNr: {color: '#999999'},
-    ColorColumn: {backgroundColor: '#eeeeee'},
-    Pmenu: {backgroundColor: '#eeeeee'},
-    PmenuSel: {backgroundColor: '#dddddd'},
-    ErrorMsg: {color: '#cc3333'},
-    MoreMsg: {},
-    ModeMsg: {},
+    Title: {},
+    Todo: {highlight: 'standout'},
+    Type: {color: '#777777'},
+    Underlined: {},
+    VertSplit: {color: '#eeeeee'},
+    Visual: {backgroundColor: '#dddddd'},
+    VisualNOS: {},
     WarningMsg: {},
-    Todo: {highlight: 'standout'}
+    WildMenu: {backgroundColor: '#dddddd'},
+    lCursor: {}
+  },
+  dark: {
+    ColorColumn: {backgroundColor: '#222222'},
+    Comment: {color: '#555555'},
+    Conceal: {color: '#999999'},
+    Conditional: {color: '#999999'},
+    Constant: {color: '#777777'},
+    Cursor: {highlight: 'reverse'},
+    CursorColumn: {backgroundColor: '#222222'},
+    CursorLine: {backgroundColor: '#222222'},
+    CursorLineNr: {color: '#777777'},
+    DiffAdd: {},
+    DiffChange: {},
+    DiffDelete: {color: '#cc3333'},
+    DiffText: {},
+    Directory: {color: '#999999'},
+    Error: {},
+    ErrorMsg: {color: '#cc3333'},
+    FoldColumn: {color: '#555555'},
+    Folded: {color: '#555555'},
+    Ignore: {},
+    IncSearch: {backgroundColor: '#555555'},
+    LineNr: {color: '#555555'},
+    MatchParen: {backgroundColor: '#555555'},
+    ModeMsg: {},
+    MoreMsg: {},
+    NonText: {color: '#555555'},
+    Normal: {
+      color: '#aaaaaa',
+      backgroundColor: '#000000'
+    },
+    Number: {color: '#777777'},
+    Pmenu: {backgroundColor: '#222222'},
+    PmenuSbar: {},
+    PmenuSel: {backgroundColor: '#333333'},
+    PmenuThumb: {},
+    Question: {},
+    Search: {backgroundColor: '#333333'},
+    SignColumn: {color: '#555555'},
+    Special: {color: '#999999'},
+    SpecialKey: {color: '#555555'},
+    SpellBad: {color: '#cc3333'},
+    SpellCap: {},
+    SpellLocal: {},
+    SpellRare: {},
+    Statement: {color: '#999999'},
+    StatusLine: {
+      color: '#999999',
+      backgroundColor: '#222222'
+    },
+    StatusLineNC: {
+      color: '#777777',
+      backgroundColor: '#222222'
+    },
+    StorageClass: {color: '#999999'},
+    String: {color: '#777777'},
+    TabLine: {
+      color: '#777777',
+      backgroundColor: '#222222'
+    },
+    TabLineFill: {backgroundColor: '#222222'},
+    TabLineSel: {
+      color: '#999999',
+      backgroundColor: '#222222'
+    },
+    Title: {},
+    Todo: {highlight: 'standout'},
+    Type: {color: '#999999'},
+    Underlined: {},
+    VertSplit: {color: '#333333'},
+    Visual: {backgroundColor: '#555555'},
+    VisualNOS: {},
+    WarningMsg: {},
+    WildMenu: {backgroundColor: '#333333'},
+    lCursor: {}
   }
 };
 module.exports = initialState;
