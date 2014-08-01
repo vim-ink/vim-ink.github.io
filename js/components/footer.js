a1f01f9a16e266833ef5f7216620dadb29386de2
Alexander Teinum <ateinum@gmail.com>
2014-08-01 20:49:58 +0200

    Add link to Twitter

--- js/components/footer.js
+++ js/components/footer.js
@@ -11,6 +11,8 @@ var Footer = React.createClass({
                         this.props.setParsedSource(files.about.parsedSource);
                      }}, 'About')),
                 React.DOM.li(null,
+                     React.DOM.a({href: 'https://twitter.com/vim_ink'}, 'Twitter')),
+                React.DOM.li(null,
                      React.DOM.a({href: 'https://github.com/vim-ink/vim-ink.github.io'}, 'GitHub')),
                 React.DOM.li(null,
                      React.DOM.a({href: 'https://www.gittip.com/vim_ink/'}, 'Donate'))));
