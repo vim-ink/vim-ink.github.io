a917f939cc5ca2e05b36f6e0369cb597a3d2a2d4
Alexander Teinum <ateinum@gmail.com>
2014-08-01 20:15:54 +0200

    Fix links

--- js/components/footer.js
+++ js/components/footer.js
@@ -11,9 +11,9 @@ var Footer = React.createClass({
                         this.props.setParsedSource(files.about.parsedSource);
                      }}, 'About')),
                 React.DOM.li(null,
-                     React.DOM.a({href: 'http://github.com/alexanderte'}, 'GitHub')),
+                     React.DOM.a({href: 'https://github.com/vim-ink/vim-ink.github.io'}, 'GitHub')),
                 React.DOM.li(null,
-                     React.DOM.a({href: ''}, 'Donate'))));
+                     React.DOM.a({href: 'https://www.gittip.com/vim_ink/'}, 'Donate'))));
     }
 });
 
