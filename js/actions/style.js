ea9c77db23e2b69ff805b1d1f4e769416f7479cd
Alexander Teinum <ateinum@gmail.com>
2014-08-03 21:17:58 +0200

    Fix font stuff

--- js/actions/style.js
+++ js/actions/style.js
@@ -8,7 +8,7 @@ function getGroupStyle(normal, group, postProcess) {
 
     switch (group.highlight) {
         case 'bold':
-            style['fontFamily'] = 'Input Mono Narr';
+            style['fontFamily'] = 'Input Mono Narr Md';
             break;
         case 'italic':
             style['fontStyle'] = 'italic';
@@ -26,7 +26,7 @@ function getGroupStyle(normal, group, postProcess) {
             backgroundColor = color_ !== undefined ? color_ : normal.color;
             break;
         case 'standout':
-            style['fontFamily'] = 'Input Mono Narr';
+            style['fontFamily'] = 'Input Mono Narr Md';
             var color_ = color;
 
             color = backgroundColor !== undefined ? backgroundColor : normal.backgroundColor;
